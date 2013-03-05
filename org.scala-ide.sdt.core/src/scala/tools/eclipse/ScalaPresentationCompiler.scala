99e188489e10d3be6269241f6ff95da5a2409634
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2013-03-05 15:41:18 +0400

    Interactive scaladoc.
    Add interactive scaladoc used when showing the hover and
    when presenting scala symbol in the completion popup.
    
    Fix #1000453, Fix #1000210.
    
    Conflicts:
    	org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/completion/CompletionTests.scala
    	org.scala-ide.sdt.core.tests/src/scala/tools/eclipse/testsetup/TestProjectSetup.scala
    	org.scala-ide.sdt.core/src/scala/tools/eclipse/LocateSymbol.scala
    	org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaHover.scala
    	org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaPresentationCompiler.scala
    	org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/ScalaCompletionProposal.scala
    	org.scala-ide.sdt.core/src/scala/tools/eclipse/ui/ScalaCompletionProposalComputer.scala
    	pom.xml

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaPresentationCompiler.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaPresentationCompiler.scala
@@ -61,7 +61,10 @@ class ScalaPresentationCompiler(val project: ScalaProject, settings: Settings) e
   with JavaSig
   with JVMUtils
   with LocateSymbol
-  with HasLogger { self =>
+  with HasLogger
+  with Scaladoc { self =>
+
+  override def forScaladoc = true
 
   def presentationReporter = reporter.asInstanceOf[ScalaPresentationCompiler.PresentationReporter]
   presentationReporter.compiler = this
@@ -323,6 +326,8 @@ class ScalaPresentationCompiler(val project: ScalaProject, settings: Settings) e
       } else scalaParamNames
     }
 
+    val docFun = () => browserInput(sym, sym.enclClass) // TODO: proper site. How?
+
     CompletionProposal(kind,
       start,
       name,
@@ -333,7 +338,8 @@ class ScalaPresentationCompiler(val project: ScalaProject, settings: Settings) e
       getParamNames,
       paramTypes,
       sym.fullName,
-      false)
+      false,
+      docFun)
   }
 
   override def inform(msg: String): Unit =
