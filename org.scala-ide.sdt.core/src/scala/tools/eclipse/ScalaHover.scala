19d3aed06ba25f6dbdf61133030d1433aee6b59d
François Garillot <francois@garillot.net>
2013-08-26 09:42:34 +0200

    Minor fix
    absent return type of getHOverInfo breaks the play2 plugin

--- org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaHover.scala
+++ org.scala-ide.sdt.core/src/scala/tools/eclipse/ScalaHover.scala
@@ -26,7 +26,7 @@ class ScalaHover(val icu: InteractiveCompilationUnit) extends ITextHover with IT
   private val NoHoverInfo = "" // could return null, but prefer to return empty (see API of ITextHover).
 
   @deprecated("Use getHoverInfo2","4.0.0")
-  override def getHoverInfo(viewer: ITextViewer, region: IRegion) = null
+  override def getHoverInfo(viewer: ITextViewer, region: IRegion) : String = null
 
   override def getHoverInfo2(viewer: ITextViewer, region: IRegion): Object =
     icu.withSourceFile({ (src, compiler) =>
